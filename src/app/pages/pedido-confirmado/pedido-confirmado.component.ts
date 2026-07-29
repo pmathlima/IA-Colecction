aa54674ef82127f5a6ac0affc4eb26023c0f9303
Ponciano Matheus <poncimatheuslima@gmail.com>
2026-07-29 18:51:53 -0300

    feat: melhorar tela de pedido confirmado

--- src/app/pages/pedido-confirmado/pedido-confirmado.component.ts
+++ src/app/pages/pedido-confirmado/pedido-confirmado.component.ts
@@ -1,21 +1,27 @@
-import { DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit, inject, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { DatePipe } from "@angular/common";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+  signal,
+} from "@angular/core";
+import { RouterLink } from "@angular/router";
 
-import { ORDER_STATUS_LABELS, Order } from '../../core/models/order.model';
-import { CustomerAuthService } from '../../core/services/customer-auth.service';
-import { WhatsappService } from '../../core/services/whatsapp.service';
-import { UiButtonComponent } from '../../shared/components/ui-button/ui-button.component';
-import { BrlCurrencyPipe } from '../../shared/pipes/brl-currency.pipe';
+import { ORDER_STATUS_LABELS, Order } from "../../core/models/order.model";
+import { CustomerAuthService } from "../../core/services/customer-auth.service";
+import { WhatsappService } from "../../core/services/whatsapp.service";
+import { UiButtonComponent } from "../../shared/components/ui-button/ui-button.component";
+import { BrlCurrencyPipe } from "../../shared/pipes/brl-currency.pipe";
 
-const LAST_ORDER_STORAGE_KEY = 'ia-collection-last-order';
+const LAST_ORDER_STORAGE_KEY = "ia-collection-last-order";
 
 @Component({
-  selector: 'app-pedido-confirmado',
+  selector: "app-pedido-confirmado",
   standalone: true,
   imports: [RouterLink, DatePipe, UiButtonComponent, BrlCurrencyPipe],
-  templateUrl: './pedido-confirmado.component.html',
-  styleUrl: './pedido-confirmado.component.scss',
+  templateUrl: "./pedido-confirmado.component.html",
+  styleUrl: "./pedido-confirmado.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PedidoConfirmadoComponent implements OnInit {
